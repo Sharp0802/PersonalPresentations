0aa69ac5e612d09f05a77c9543d9e7f3f773adab
Sharp0802 <sharp0802.business@gmail.com>
2023-03-28 22:28:04 +0900

    Change result format to PDF

--- build-all.sh
+++ build-all.sh
@@ -2,13 +2,15 @@
 
 pre=$pwd
 cd $(dirname $(readlink -f $0))
+
+rm -rf build
 cd post
 
 set -- $(find -name '*.md')
 while [ $# -gt 0 ]; do
     fname="$1"
-    fname="${fname%.*}.html"
-    marp $1 --html --output "../build/$fname"
+    fname="${fname%.*}.pdf"
+    marp $1 --html --pdf --allow-local-files --output "../build/$fname"
 shift; done
 
 cd $pre
